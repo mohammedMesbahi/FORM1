ddf4c6a5cad37d37d777e2570e13f485fc0502c0
ed mesbahi <mesbahi.ed@outlook.com>
2022-12-12 22:33:47 +0100

    add the first panel

--- src/packegOne/Form1.java
+++ src/packegOne/Form1.java
@@ -1,4 +1,26 @@
 package packegOne;
 
-public class Form1 {
+import javax.swing.*;
+
+public class Form1 extends JFrame{
+    private JLabel lblCountry;
+    private JTextField textFieldCountry;
+    private JLabel lblTeamName;
+    private JTextField textFieldTeamName;
+    private JComboBox comboBoxCompetations;
+    private JButton btnSubmit;
+    private JLabel lblCompetationName;
+    private JPanel mainPanel;
+
+    public Form1(){
+        setTitle("Main Frame");
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,400);
+        this.pack();
+    }
+    public static void main(String[] args){
+        JFrame frame = new Form1();
+        frame.setVisible(true);
+    }
 }
